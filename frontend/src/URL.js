60a195b79d3c1678cc937d58f1f4d311a3001430
Roshan <roshan20031112@gmail.com>
2025-08-05 18:09:14 +0530

    url added

--- frontend/src/URL.js
+++ frontend/src/URL.js
@@ -1,3 +1,3 @@
-export const url="http://localhost:5000/api"
+export const url="https://unlok.onrender.com"
 // export const url="https://62t7qrw9-5000.inc1.devtunnels.ms/api"
 // export const url="http://192.168.31.30:5000/api"
