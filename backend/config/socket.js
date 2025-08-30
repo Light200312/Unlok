da54fee64a9475f978525e99963b9d8ae7c15279
Roshan <roshan20031112@gmail.com>
2025-08-30 20:17:24 +0530

    added on 30/8/25

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: ["http://localhost:5174", "http://localhost:5173","https://unlok-lkr3.onrender.com"],
   },
 });
 
